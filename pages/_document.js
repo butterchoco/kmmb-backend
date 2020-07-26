7af5b928f9a523898cc780cc105a7fd16d8894d7
butterchoco <ahmadsupri1999@gmail.com>
2020-07-26 23:57:56 +0700

    Add firebase configuration by yarn

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,6 @@ class KmmbDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <script src="/__/firebase/7.17.1/firebase-app.js"></script>
-          <script src="/__/firebase/7.17.1/firebase-analytics.js"></script>
-          <script src="/__/firebase/init.js"></script>
         </Head>
         <body>
           <Main />
